c88a5c5ed3ba1d4e64ed40d8748321718036839a
Abhishek Jariwala <ajariwala@auburn.edu>
2023-01-22 06:36:01 -0600

    Initial Commit

---
+++ QuizApp_Completed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp_Completed
+//
+//  Created by Abhishek Jariwala on 1/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
